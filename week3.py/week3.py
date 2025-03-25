91b76942096c92b45e8ec964c7b9359ff9741ac5
rkoech362 <kiprokoch@gmail.com>
2025-03-25 22:09:32 +0300

    first commit

---
+++ week3.py/week3.py
@@ -0,0 +1,11 @@
+my_list=[1,2,3,4,5,6,7,8,9]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list[1]=15
+my_list.extend([50,60,70])
+my_list.pop(-1)
+my_list.sort()
+index=my_list.index(30)
+print(index)
